94fcf2fbcca7182b20a1a58f9997e900d1013773
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2019-02-16 22:56:29 +0200

    Set enviroment variables for YARN

--- files/profile.d/set-hadoop-prefix.sh
+++ files/profile.d/set-hadoop-prefix.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 export HADOOP_PREFIX=/usr/local/hadoop
+
 export HADOOP_CONF_DIR=/etc/hadoop
 export HADOOP_LOG_DIR=/var/local/hadoop/logs
+
+export YARN_CONF_DIR=${HADOOP_CONF_DIR} 
+export YARN_LOG_DIR=${HADOOP_LOG_DIR}
 
 PATH=${PATH}:/usr/local/hadoop/bin:/usr/local/hadoop/sbin
